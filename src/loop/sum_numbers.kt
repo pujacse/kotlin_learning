28d0fd4243a7206d8eac480e08d7356074210340
pujacse <122685894+pujacse@users.noreply.github.com>
2025-02-01 21:36:33 +0600

    Create sum_numbers.kt

---
+++ src/loop/sum_numbers.kt
@@ -0,0 +1,21 @@
+/* Write a program to calculate the sum of the first n natural numbers using a while loop.*/
+
+fun main() {
+    print("Enter a positive integer: ")
+    val n = readLine()?.toIntOrNull() ?: 0
+
+    if (n <= 0) {
+        println("Please enter a valid positive integer.")
+        return
+    }
+
+    var sum = 0
+    var i = 1
+
+    while (i <= n) {
+        sum += i
+        i++
+    }
+
+    println("The sum of the first $n natural numbers is: $sum")
+}
