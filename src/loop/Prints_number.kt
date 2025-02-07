1b3c63766f2bb75b3b1c0433f179d72b12a6f640
Puja Das <pujacse23@gmail.com>
2025-02-07 21:57:12 +0600

    added - print number with loop problem

---
+++ src/loop/Prints_number.kt
@@ -0,0 +1,9 @@
+/* Write a Kotlin program that prints numbers from 1 to 10 using a while loop. */
+
+fun main() {
+    var num = 1
+    while (num <= 10) {
+        println(num)
+        num++
+    }
+}
