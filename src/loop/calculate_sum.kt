7e0a3bcb701318582f47e3fcbe0adae1d843c077
Puja Das <pujacse23@gmail.com>
2025-02-07 21:51:10 +0600

    added - print number with loop problem

--- src/loop/calculate_sum.kt
+++ src/loop/calculate_sum.kt
@@ -1,13 +1,12 @@
-package loop
+/*Write a Kotlin program that uses a for loop to find the
+sum of all even numbers from 1 to 50.*/
 
-fun main()
-{
+fun main() {
     var sum = 0
 
-    for(num in 1..50 step 2)
-    {
+    for (num in 2..50 step 2) {
         sum += num
     }
 
-    println("sum number: $sum")
+    println("Sum of even numbers from 1 to 50 is: $sum")
 }
