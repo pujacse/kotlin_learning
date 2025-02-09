deea356589d66895643184543445104fcca13b96
Puja Das <pujacse23@gmail.com>
2025-02-09 23:35:52 +0600

    add

--- src/loop/Reverse_digit.kt
+++ src/loop/Reverse_digit.kt
@@ -1,7 +1,14 @@
-fun main()
-{
-    println("Enter a number: ")
-    val n = readLine()?.toIntOrNull()
+fun main() {
+    print("Enter a number: ")
+    val num = readLine()?.toIntOrNull() ?: return  // Read input and handle null case
+    var originalNum = num
+    var reversedNum = 0
 
-    while ()
+    while (originalNum > 0) {
+        val digit = originalNum % 10  // Get the last digit
+        reversedNum = reversedNum * 10 + digit  // Append digit to reversed number
+        originalNum /= 10  // Remove last digit
+    }
+
+    println("Reversed number: $reversedNum")
 }
